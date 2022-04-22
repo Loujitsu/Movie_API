8b97b5a9035fd2a2f66f4e78b85242feb65787b2
Loujitsu <53363924+Loujitsu@users.noreply.github.com>
2022-04-21 18:03:12 -0700

    added top 10 movies
    also added express.static and server port

--- index.js
+++ index.js
@@ -1,1 +1,79 @@
+const express = require('express'),
+    morgan = require('morgan');
+
+const app = express();
+
+let topMovies = [
+    {
+      title: 'Harry Potter and the Sorcerer\'s Stone',
+      year: '2001'
+    },
+    {
+      title: 'Lord of the Rings',
+      year: '2003'
+    },
+    {
+      title: 'Fight Club',
+      year: '1999'
+    },
+    {
+      title: 'Fifth Element',
+      year: '1997'     
+    },
+    {
+      title: 'Mad Max',
+      year: '2015'
+    },
+    {
+      title: 'The Martian',
+      year: '2015'
+    },
+    {
+      title: 'Alien: Covenant',
+      year: '2017'
+    },
+    {
+      title: 'Dune',
+      year: '2021'
+    },
+    {
+      title: 'Predator',
+      year: '1987'
+    },
+    {
+      title: 'Moneyball',
+      year: '2011'
+    }
+  ];
+  
+  // GET requests
+  app.get('/', (req, res) => {
+    res.send('Welcome to my movies club!');
+  });
+  
+  app.get('/documentation', (req, res) => {                  
+    res.sendFile('public/documentation.html', { root: __dirname });
+  });
+  
+  app.get('/movies', (req, res) => {
+    res.json(topMovies);
+  });
+  
+//   Serve static content from the public directory
+  app.use(express.static('public'));
+
+  app.use((err, req, res, next) => {
+      console.error(err.stack);
+      res.status(500).send('Something went wrong!');
+  });
+
+
+
+
+  // listen for requests
+  app.listen(8080, () => {
+    console.log('Your app is listening on port 8080.');
+  });
+
+
 
