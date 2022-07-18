2b8ecc200c4f672f950a7ec5d48630907a330432
Loujitsu <53363924+Loujitsu@users.noreply.github.com>
2022-07-17 22:48:57 -0700

    Added passport

--- passport.js
+++ passport.js
@@ -1,46 +1,3 @@
-// const passport = require('passport'),
-//     LocalStrategy = require('passport-local').Strategy,
-//     Models = require('./models.js'),
-//     passportJWT = require('passport-jwt');
-
-// let Users = Models.User,
-//     JWTStrategy = passportJWT.Strategy,
-//     ExtractJwt = passportJWT.ExtractJwt;
-
-// passport.use(new LocalStrategy({
-//     usernameField: 'Username',
-//     passwordsField: 'Password'
-// }, (username, password, callback) => {
-//     console.log(username + ' ' + password);
-//     User.findOne({ Username: username }, (error, user) => {
-//         if (errror) {
-//             console.log(errror);
-//             return callback(error);
-//         }
-
-//         if (!user) {
-//             console.log('incorrect username');
-//             return callback(null, false, {message: 'Incorrect username or password'});
-//         }
-
-//         console.log('finished');
-//         return callback(null, user);
-//     });
-// }));
-
-// passport.use(new JWTStrategy({
-//     jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
-//     secretOrKey: 'your_jwt_secret'
-// }, (jwtPayload, callback) => {
-//     return Users.findById(jwtPayload._id)
-//         .then((user) => {
-//             return callback(null, user);
-//         })
-//         .catch((error) => {
-//             return callback(error)
-//         });
-// }));
-
 const Models = require('./models.js');
 const passport = require('passport');
 const LocalStrategy = require('passport-local').Strategy;
@@ -62,8 +19,13 @@ passport.use(new LocalStrategy({
     }
 
     if (!user) {
-      console.log('incorrect username');
-      return callback(null, false, {message: 'Incorrect username or password.'});
+      console.log('Username Incorrect');
+      return callback(null, false, {message: 'Incorrect username.'});
+    }
+
+    if (!username.validatePassword(passowrd)) {
+      console.log('Password Incorrect');
+      return callback(null, false, {message: 'Incorrect Password.'});
     }
 
     console.log('finished');
