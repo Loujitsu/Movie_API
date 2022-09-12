026ff2a3f2b6f77f0286d76f72dc8fe029f3affe
Luis O <zenodro1588@gmail.com>
2022-09-12 01:05:25 -0700

    Restored Code

--- index.js
+++ index.js
@@ -1,37 +1,60 @@
 const express = require('express'),
-    morgan = require('morgan');
-    bodyParser = require('body-parser');
+    morgan = require('morgan'),
+    bodyParser = require('body-parser'),
     uuid = require('uuid');
-    cors = require('cors');
+
+const mongoose = require('mongoose');
+const Models = require('./models.js/models.js');
+
+const Movies = Models.Movies;
+const Users = Models.Users;
+const Genres = Models.Genre;
+const Directors = Models.Director;
+// Change this to your connection string from MonngoAtlas the remote one
+// mongoose.connect('mongodb://localhost:27017/CineAPI', { useNewUrlParser: true, useUnifiedTopology: true });
+// Change <password> to your password
+// Redeploy to heroku
+mongoose.connect(process.env.CONNECTION_URI, { useNewUrlParser: true, useUnifiedTopology: true });
 
 const app = express();
+
 app.use(morgan('common'));
 app.use(bodyParser.json());
-app.use(cors());
 
-let allowedOrigins = ['http://localhost:8080', 'http://testsite.com', 'http://localhost:1234'];
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
+
+const passport = require('passport');
+require('./passport');
+
+const { check, validationResult } = require('express-validator');
+
+const cors = require('cors');
+const { request } = require('express');
+
+let allowedOrigins = ['http://localhost:8888', 'http://testsite.com', 'https://movie-flix-289893.herokuapp.com/', 'http://localhost:1234'];
 
 app.use(cors({
   origin: (origin, callback) => {
     if(!origin) return callback(null, true);
-    if(allowedOrigins.indexOf(origin) === -1){ // If a specific origin isn’t found on the list of allowed origins
-      let message = 'The CORS policy for this application doesn’t allow access from origin ' + origin;
+    if(allowedOrigins.indexOf(origin) === -1){ //If a specific origin isn't found on found on the of allowed origins
+      let message = 'The CORS policy for this application does not allow access from origin' + origin;
       return callback(new Error(message ), false);
     }
     return callback(null, true);
   }
 }));
 
-app.use(bodyParser.urlencoded({
-  extended: true
-}));
+let auth = require('./auth')(app);
 
 let topMovies = [
     {
       "Title": 'The Martian',
       "Description": "An astronaut becomes stranded on Mars after his team assume him dead, and must rely on his ingenuity to find a way to signal to Earth that he is alive and can survive until a potential rescue.",
       "Genre": {
-          "Name": "Sci-Fi"
+          "Name": "Sci-Fi",
+          "Description" : "A film genre that uses speculative, fictional science-based depictions of phenomena that are not fully accepted by mainstream science, such as extraterrestrial lifeforms, spacecraft, robots, cyborgs, interstellar travel or other technologies."
       },
       "Director": {
           "Name": 'Ridley Scott',
@@ -44,7 +67,8 @@ let topMovies = [
       "Title": 'Predator',
       "Description": "A team of commandos on a mission in a Central American jungle find themselves hunted by an extraterrestrial warrior.",
       "Genre": {
-          "Name": "Action"
+          "Name": "Action",
+          "Description" : "A film genre where action sequences, such as fighting, stunts, car chases or explosions, take precedence over elements like characterization or complex plotting. The action typically involves individual efforts on the part of the hero, in contrast with most war films."
       },
       "Director": {
           "Name": 'John McTiernan',
@@ -57,7 +81,8 @@ let topMovies = [
       "Title": 'Dune',
       "Description": "A noble family becomes embroiled in a war for control over the galaxys most valuable asset while its heir becomes troubled by visions of a dark future.",
       "Genre": {
-          "Name": "Drama"
+          "Name": "Drama",
+          "Description" : "The drama genre features stories with high stakes and a lot of conflicts. They're plot-driven and demand that every character and scene move the story forward."
       },
       "Director": {
           "Name": 'Denis Villeneuve',
@@ -70,7 +95,8 @@ let topMovies = [
       "Title": 'The Gentlemen',
       "Description": "An American expat tries to sell off his highly profitable marijuana empire in London, triggering plots, schemes, bribery and blackmail in an attempt to steal his domain out from under him.",
       "Genre": {
-          "Name": "Action"
+          "Name": "Action",
+          "Description" : "A film genre where action sequences, such as fighting, stunts, car chases or explosions, take precedence over elements like characterization or complex plotting. The action typically involves individual efforts on the part of the hero, in contrast with most war films."
       },
       "Director": {
           "Name": 'Guy Ritchie',
@@ -83,7 +109,8 @@ let topMovies = [
       "Title": 'The Dark Knight',
       "Description": "When the menace known as the Joker wreaks havoc and chaos on the people of Gotham, Batman must accept one of the greatest psychological and physical tests of his ability to fight injustice.",
       "Genre": {
-          "Name": "Action"
+          "Name": "Action",
+          "Description" : "A film genre where action sequences, such as fighting, stunts, car chases or explosions, take precedence over elements like characterization or complex plotting. The action typically involves individual efforts on the part of the hero, in contrast with most war films."
       },
       "Director": {
           "Name": 'Christopher Nolan',
@@ -96,7 +123,8 @@ let topMovies = [
       "Title": 'Deadpool',
       "Description": "A wisecracking mercenary gets experimented on and becomes immortal but ugly, and sets out to track down the man who ruined his looks.",
       "Genre": {
-          "Name": "Comedy"
+          "Name": "Comedy",
+          "Description" : "Comedies are light-hearted dramas, crafted to amuse, entertain, and provoke enjoyment. The comedy genre humorously exaggerates the situation, the language, action, and characters."
       },
       "Director": {
           "Name": 'Tim Miller',
@@ -109,7 +137,8 @@ let topMovies = [
       "Title": 'No Country For Old Men',
         "Description": "Violence and mayhem ensue after a hunter stumbles upon a drug deal gone wrong and more than two million dollars in cash near the Rio Grande.",
         "Genre": {
-            "Name": "Thriller"
+            "Name": "Thriller",
+            "Description" : "Thrillers are characterized and defined by the moods they elicit, giving viewers heightened feelings of suspense, excitement, surprise, anticipation and anxiety."
         },
         "Director": {
             "Name":'Coen\'s',
@@ -122,7 +151,8 @@ let topMovies = [
       "Title": 'Knives Out',
       "Description": "A detective investigates the death of the patriarch of an eccentric, combative family.",
       "Genre": {
-          "Name": "Crime"
+          "Name": "Crime",
+          "Description" : "A film genre inspired by and analogous to the crime fiction literary genre. Films of this genre generally involve various aspects of crime and its detection."
       },
       "Director": {
           "Name": 'Rian Johnson',
@@ -132,23 +162,25 @@ let topMovies = [
       "Featured": false
     },
     {
-      "Title": 'Ready Player One',
-        "Description": "When the creator of a virtual reality called the OASIS dies, he makes a posthumous challenge to all OASIS users to find his Easter Egg, which will give the finder his fortune and control of his world",
+      "Title": 'Matrix',
+        "Description": "When a beautiful stranger leads computer hacker Neo to a forbidding underworld, he discovers the shocking truth--the life he knows is the elaborate deception of an evil cyber-intelligence.",
         "Genre": {
-            "Name": "Sci-Fi"
+            "Name": "Sci-Fi",
+            "Description" : "A film genre that uses speculative, fictional science-based depictions of phenomena that are not fully accepted by mainstream science, such as extraterrestrial lifeforms, spacecraft, robots, cyborgs, interstellar travel or other technologies."
         },
         "Director": {
-            "Name": 'Steven Spielberg',
-            "Bio": "Steven Allan Spielberg is an American film director, producer, and screenwriter. He began his career in the New Hollywood era and is currently the most commercially successful director of all time"
+            "Name": 'Wachowski\'s',
+            "Bio": "Lana Wachowski and her sister Lilly Wachowski, also known as the Wachowskis, are the duo behind such ground-breaking movies as The Matrix (1999) and Cloud Atlas (2012). Born to mother Lynne, a nurse, and father Ron, a businessman of Polish descent, Wachowski grew up in Chicago and formed a tight creative relationship with her sister Lilly. After the siblings dropped out of college, they started a construction business and wrote screenplays. Their 1995 script, Assassins (1995), was made into a movie, leading to a Warner Bros contract. After that time, the Wachowskis devoted themselves to their movie careers. In 2012, during interviews for Cloud Atlas and in her acceptance speech for the Human Rights Campaign's Visibility Award, Lana spoke about her experience of being a transgender woman, sacrificing her much cherished anonymity out of a sense of responsibility. Lana is known to be extremely well-read, loves comic books and exploring ideas of imaginary worlds, and was inspired by Stanley Kubrick's 2001: A Space Odyssey (1968) in creating Cloud Atlas. Director, writer, and producer Lilly Wachowski was born in 1967 in Chicago, the daughter of Lynne, a nurse and painter, and Ron, a businessman. Lilly was educated at Kellogg Elementary School in Chicago, before moving on to Whitney M. Young High School. After graduating from high school, she attended Emerson College in Boston but dropped out."
         },
-        "ImageURL": "https://www.imdb.com/title/tt1677720/mediaviewer/rm4286860032/?ref_=tt_ov_i",
+        "ImageURL": "https://www.imdb.com/title/tt0133093/mediaviewer/rm525547776/",
         "Featured": false
     },
     {
       "Title": 'Interstellar',
       "Description": "A team of explorers travel through a wormhole in space in an attempt to ensure humanitys survival.",
       "Genre": {
-          "Name": "Sci-fi"
+          "Name": "Sci-fi",
+          "Description" : "A film genre that uses speculative, fictional science-based depictions of phenomena that are not fully accepted by mainstream science, such as extraterrestrial lifeforms, spacecraft, robots, cyborgs, interstellar travel or other technologies."
       },
       "Director": {
           "Name":'Christopher Nolan',
@@ -173,32 +205,98 @@ let topMovies = [
 ]
 
 // Create
-app.post('/users', (req, res) => {
-    const newUser = req.body;
+app.post('/users',
+  // Validation logic here for request
+  //you can either use a chain of methods like .not().isEmpty()
+  //which means "opposite of isEmpty" in plain english "is not empty"
+  //or use .isLength({min: 5}) which means
+  //minimum value of 5 characters are only allowed
+  [
+    check('Username', 'Username is required').isLength({min: 5}),
+    check('Username', 'Username contains non alphanumeric characters - not allowed.').isAlphanumeric(),
+    check('Password', 'Password is required').not().isEmpty(),
+    check('Email', 'Email does not appear to be valid').isEmail()
+  ], (req, res) => {
 
-    if (newUser.name) {
-        newUser.id = uuid.v4();
-        users.push(newUser);
-        res.status(201).json(newUser)
-    } else {
-        res.status(400).send('users need name')
+  // check the validation object for errors
+    let errors = validationResult(req);
+
+    if (!errors.isEmpty()) {
+      return res.status(422).json({ errors: errors.array() });
     }
-})
+
+  let hashedPassword = Users.hashPassword(req.body.Password);
+  Users.findOne({ Username: req.body.Username })//Search to see if a user with the requested username is already exists
+    .then((user) => {
+      if (user) {
+        //If the user is found, send a response that it already exists
+        return res.status(400).send(req.body.Username + 'Already Exists');
+      } else {
+        Users
+          .create({
+            Username: req.body.Username,
+            Password: hashedPassword,
+            Email: req.body.Email,
+            Birthday: req.body.Birthday
+          })
+        .then((user) => { res.status(201).json(user) })
+        .catch((error) => {
+          console.error(error);
+          res.status(500).send('Error: ' + error);
+        });
+      }
+    })
+    .catch((error) => {
+      console.error(error);
+      res.status(500).send('Error: ' + error);
+    });
+});
+
+
+// Get all users
+app.get('/users', (req, res) => {
+  Users.find()
+    .then((users) => {
+      res.status(201).json(users);
+    })
+    .catch((err) => {
+      console.error(err);
+      res.status(500).send('Error: ' + err);
+    });
+});
+
+// Get a user by username
+app.get('/users/:Username', (req, res) => {
+  Users.findOne({ Username: req.params.Username })
+    .then((user) => {
+      res.json(user);
+    })
+    .catch((err) => {
+      console.error(err);
+      res.status(500).send('Error: ' + err);
+    });
+});
 
 // Update
-app.put('/users/:id', (req, res) => {
-    const { id } = req.params;
-    const updatedUser = req.body;
-
-    let user = users.find( user => user.id == id); 
-    // double == means a string value like '2' can equal integer value 2
-    if (user) {
-        user.name = updatedUser.name;
-        res.status(200).json(user);
-    } else {
-        res.status(400).send('no such user')
+app.put('/users/:Username', (req, res) => {
+  Users.findOneAndUpdate({ Username: req.params.Username }, { $set:
+    {
+      Username: req.body.Username,
+      Password: req.body.Password,
+      Email: req.body.Email,
+      Birthday: req.body.Birthday
     }
-})
+  },
+  { new: true }, // This line makes sure that the updated document is returned
+  (err, updatedUser) => {
+    if(err) {
+      console.error(err);
+      res.status(500).send('Error: ' + err);
+    } else {
+      res.json(updatedUser);
+    }
+  });
+});
 
 // Create
 app.post('/users/:id/:movieTitle', (req, res) => {
@@ -214,6 +312,22 @@ app.post('/users/:id/:movieTitle', (req, res) => {
     }
 })
 
+// Add a movie to a user's list of favorites
+app.post('/users/:Username/movies/:MovieID', (req, res) => {
+  Users.findOneAndUpdate({ Username: req.params.Username }, {
+     $push: { FavoriteMovies: req.params.MovieID }
+   },
+   { new: true }, // This line makes sure that the updated document is returned
+  (err, updatedUser) => {
+    if (err) {
+      console.error(err);
+      res.status(500).send('Error: ' + err);
+    } else {
+      res.json(updatedUser);
+    }
+  });
+});
+
 // Delete
 app.delete('/users/:id/:movieTitle', (req, res) => {
     const { id, movieTitle } = req.params;
@@ -228,19 +342,38 @@ app.delete('/users/:id/:movieTitle', (req, res) => {
     }
 })
 
-// Delete
-app.delete('/users/:id', (req, res) => {
-    const { id } = req.params;
+app.delete('/users/:Username/movies/:MovieID', (req, res,) => {
+  Users.findOneAndUpdate({ Username: req.params.Username }, {
+    $pull: { FavoriteMovies: req.params.MovieID },
+    }, 
+    { new: true },
+    (err, updatedUser) => {
+      if (err) {
+        console.error(err);
+        res.status(500).send('Error: ' + err);
+      } else {
+        res.json(updatedUser);
+      }
+    });
+});  
 
-    let user = users.find( user => user.id == id); 
-    // double == means a string value like '2' can equal integer value 2
-    if (user) {
-        users = users.filter(user => user.id != id);
-        res.status(200).send('User ' + id + ' has been deleted');
-    } else {
-        res.status(400).send('no such user')
-    }
-})
+
+
+// Delete
+app.delete('/users/:Username', (req, res) => {
+  Users.findOneAndRemove({ Username: req.params.Username })
+    .then((user) => {
+      if (!user) {
+        res.status(400).send(req.params.Username + ' was not found');
+      } else {
+        res.status(200).send(req.params.Username + ' was deleted.');
+      }
+    })
+    .catch((err) => {
+      console.error(err);
+      res.status(500).send('Error: ' + err);
+    });
+});
 
 
   
@@ -248,18 +381,30 @@ app.delete('/users/:id', (req, res) => {
   app.get('/', (req, res) => {
     res.send('Welcome to CineApi!');
   });
+
   
   app.get('/documentation', (req, res) => {                  
     res.sendFile('public/documentation.html', { root: __dirname });
   });
   
-  app.get('/movies', (req, res) => {
-    res.json(topMovies);
-  });
+  // app.get('/movies', (req, res) => {
+  //   res.json(topMovies);
+  // });
   
-  // READ
-  app.get('/movies', (req, res) => {
-    res.status(200).json(topMovies);
+  // // READ
+  // app.get('/movies', (req, res) => {
+  //   res.status(200).json(topMovies);
+  // });
+
+  app.get('/movies', passport.authenticate('jwt', { session: false }), (req, res) => {
+    Movies.find()
+      .then((movies) => {
+        res.status(201).json(movies);
+      })
+      .catch((err) => {
+        console.error(err);
+        res.status(500).send('Error: ' + err);
+      });
   });
 
   // Read
@@ -312,9 +457,13 @@ app.get('/movies/director/:directorName', (req, res) => {
   });
 
   // listen for requests
-  app.listen(8888, () => {
-    console.log('Listening on port 8888.');
+  const port = process.env.PORT || 8888;
+  app.listen(port, '0.0.0.0',() => {
+   console.log('Listening on Port ' + port);
   });
 
 
+  // https://git.heroku.com/protected-earth-77833.git
 
+
+  // mongoimport --uri mongodb+srv://Lordonez:Bearandfinn88!@cinedb.yuo28.mongodb.net/moives --collection movies --type json --file ../moviesJSON/movies.joson
